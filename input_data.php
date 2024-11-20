61fbaee53b7d6d69d2708c91e75a4d31be49c652
Aditya Yudha <33762836+dityudha@users.noreply.github.com>
2024-11-20 10:09:08 +0700

    Update input_data.php
    update param

--- input_data.php
+++ input_data.php
@@ -1,17 +1,30 @@
 <?php
-// Koneksi ke database (sesuaikan dengan pengaturan database Anda)
-$host = 'mysql-poc-db.apps.ocp-gcp-poc.poctestingocp.com'; // Alamat server
-$username = 'adminpoc'; // Username database
-$password = 'adminpoc123'; // Password database
-$dbname = 'pocdb'; // Nama database yang ingin diakses
+// // Koneksi ke database (sesuaikan dengan pengaturan database Anda)
+// $host = 'mysql-poc-db.apps.ocp-gcp-poc.poctestingocp.com'; // Alamat server
+// $username = 'adminpoc'; // Username database
+// $password = 'adminpoc123'; // Password database
+// $dbname = 'pocdb'; // Nama database yang ingin diakses
 
-// Membuat koneksi
-$conn = new mysqli($host, $username, $password, $dbname);
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbuser = getenv("MYSQL_USER");
+$pwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
 
-// Mengecek koneksi
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $pwd, $dbname);
+
+// Check connection
 if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+    die("Connection failed. Please make sure you have the MYSQL_SERVICE_HOST, MYSQL_USER, MYSQL_PASSWORD, and MYSQL_DATABASE environment variables : " . $conn->connect_error);
 }
+
+// // Membuat koneksi
+// $conn = new mysqli($host, $username, $password, $dbname);
+
+// // Mengecek koneksi
+// if ($conn->connect_error) {
+//     die("Koneksi gagal: " . $conn->connect_error);
+// }
 
 // Memeriksa apakah form sudah disubmit
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
