aa9d010697d8518db4f95541187a7b463eb4720a
Muhamad Ferdiansyah <62669143+mferdix@users.noreply.github.com>
2024-11-20 10:00:00 +0700

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php 
+
+$host = '10.125.131.226'; 
+$username = 'adminpoc'; // Username database
+$password = 'adminpoc123'; // Password database
+$dbname = 'pocdb'; // Nama database yang ingin diakses
+
+// Membuat koneksi
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Mengecek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
